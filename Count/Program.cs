55e15f91bd89aa2678c4d618d431bc4359bd1bc0
berkayakcay <metruk@gmail.com>
2016-02-07 14:10:17 +0200

    Test

--- Count/Program.cs
+++ Count/Program.cs
@@ -15,7 +15,7 @@ namespace Count
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            Application.Run(new Main()); //Call main function
         }
     }
 }
